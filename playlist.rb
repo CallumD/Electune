5d9ebb5b772cc051067d782671edae1a5d7f9f8d
Callum Dryden <callumdryden@gmail.com>
2012-03-17 11:59:16 +0000

    Adding the playlist specs and writing the code to get them passing. TDD baby!

--- playlist.rb
+++ playlist.rb
@@ -1,6 +1,25 @@
 class Playlist
-  attr_accessor :songs
   def initialize
-   @songs = Hash.new
+   @songs = Array.new
+  end
+  
+  def count
+    @songs.count
+  end
+
+  def push song
+    @songs.push song
+  end
+
+  def shift
+    @songs.shift
+  end
+  
+  def delete song
+    @songs.delete song
+  end
+
+  def include? song
+    @songs.include? song
   end
 end
--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -2,55 +2,43 @@ require_relative '../playlist'
 
 describe Playlist, "#songs" do
 
-  it "should be a hash object" do
-    playlist = Playlist.new
-    playlist.songs.class.should eq(Array)
-  end
+  let(:playlist) { playlist = Playlist.new }
 
   it "should be empty when first initialised" do
-    playlist = Playlist.new
-    playlist.songs.count.should eq(0)
+    playlist.count.should eq(0)
   end
 
-  it "should not be able to writeable directly"
-    playlist = Playlist.new
-    playlist.songs << "test"
-  end
-
-  it "should add to songs via playlist"
-    playlist = Playlist.new
+  it "should add to songs via playlist" do
     playlist.push("songOne")
-    playlist.songs.count.should eq(1)
+    playlist.count.should eq(1)
   end  
 
-  it "should remove song when played"
-    playlist = Playlist.new
+  it "should remove song when played" do
     playlist.push("songOne")
-    playlist.pop.should eq("songOne")
-    playlist.songs.count.should eq(-1)
+    playlist.shift.should eq("songOne")
+    playlist.count.should eq(0)
   end
 
-  it "should preserve the order in which the songs are added"
-    playlist = Playlist.new
+  it "should preserve the order in which the songs are added" do
+    addThreeSongs
+    playlist.shift.should eq("one")
+    playlist.shift.should eq("two")
+    playlist.shift.should eq("three")
+  end
+
+  it "should be possible to remove a song" do
+    addThreeSongs
+    playlist.delete("two").should eq("two")
+    playlist.count.should eq(2)
+    playlist.include?("two").should eq(false)
+  end
+
+  def addThreeSongs
     playlist.push("one")
     playlist.push("two")
     playlist.push("three")
-    playlist.pop.should eq("one")
-    playlist.pop.should eq("two")
-    playlist.pop.should eq("three")
-  end
-
-  it "should be possible to remove a song"
-    playlist = Playlist.new        
-    playlist.push("one")
-    playlist.push("two")
-    playlist.push("three")
-    playlist.remove("two").should eq("two")
-    playlist.songs.count.should eq(2)
-    playlist.songs.encludes?("two").should eq(false)
   end
 end
-
 #song should have votes
 #vito
 #control n
