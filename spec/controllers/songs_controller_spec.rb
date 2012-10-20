9854140cb3fbdc83157985f24d9704ef3b5b82b1
Callum Dryden <callumdryden@gmail.com>
2012-10-20 22:21:10 +0100

    Got the specs passing the test on the object variable did not re hit the databse. Forced by putting a find in the arrtibute selection.

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -4,11 +4,11 @@ describe SongsController do
 
 let (:song) { Song.create name: "test", votes: 2 }
 
-  describe "upvote a song with Ajax" do
+  describe "upvote a song with Ajax" do  
     it "should increment votes count" do
       expect do
         xhr :post, :upvote, {id: song.id}
-      end.to change{ song.votes }.by(1)
+      end.to change{ Song.find(song.id).votes }.by(1)      
     end
 
     it "should respond with success" do
@@ -21,7 +21,7 @@ let (:song) { Song.create name: "test", votes: 2 }
     it "should decrement votes count" do
       expect do
         xhr :post, :veto, {id: song.id}
-      end.to change{ song.votes }.by(1)
+      end.to change{ Song.find(song.id).votes }.by(-1)
     end
 
     it "should respond with success" do
