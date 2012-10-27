5b3ba12dba85eff54973efe5b5f1c3296df7182a
Callum Dryden <callumdryden@gmail.com>
2012-10-27 17:16:22 +0100

    Fixing formatting.

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe PlaylistsController do
   describe "GET #show" do
-  it "assigns the requested playlist to @playlist" do
-    playlist = FactoryGirl.create(:playlist)
-    get :show, id: playlist
-    assigns(:playlist).should eq(playlist)
+    it "assigns the requested playlist to @playlist" do
+      playlist = FactoryGirl.create(:playlist)
+      get :show, id: playlist
+      assigns(:playlist).should eq(playlist)
+    end
+    
+    it "renders the #show view" do
+      get :show, id: FactoryGirl.create(:playlist)
+      response.should render_template :show
+    end
   end
-  
-  it "renders the #show view" do
-    get :show, id: FactoryGirl.create(:playlist)
-    response.should render_template :show
-  end
-end
 end
