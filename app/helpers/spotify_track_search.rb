c7eb792b581b0f13cb335c3637a54e5219418e0b
Callum Dryden <callumdryden@gmail.com>
2012-11-17 11:37:56 +0000

    Bit more tidying up.

--- app/helpers/spotify_track_search.rb
+++ app/helpers/spotify_track_search.rb
@@ -15,22 +15,25 @@ RELEASED='released'
 
   def self.perform_search search_term
     service_result = get_service_response
-    tracks = service_result[TRACKS].map do |service_track|
-      track = Track.new(name: data[NAME], length: Time.at(data[LENGTH]).utc.strftime(TIME_FORMAT),
-      spotify_link: data[HREF])
-
-      track.album = Album.new(name: data[ALBUM][NAME], release_date: data[ALBUM][RELEASED],
-      spotify_link: data[ALBUM][HREF])
-
-      track.artists = data[ARTISTS].map do |artist|
-          Artist.new(track_artist.name = artist[NAME],
-          track_artist.spotify_link = artist[HREF])
-      end
-      track
-    end
+    service_result[TRACKS].map { |service_track| build_track_from_hash service_track }
   end
 
   private
+    def build_track_from_hash service_track
+      track = Track.new(name: data[NAME], length: Time.at(data[LENGTH]).utc.strftime(TIME_FORMAT),
+      spotify_link: data[HREF])
+      track.album = Album.new(name: data[ALBUM][NAME], release_date: data[ALBUM][RELEASED],
+      spotify_link: data[ALBUM][HREF])
+      track.artists = data[ARTISTS].map { |artist| build_artist_from_hash artist }
+      track
+    end
+
+
+    def build_artist_from_hash artist
+      Artist.new(track_artist.name = artist[NAME],
+          track_artist.spotify_link = artist[HREF])
+    end
+
     def get_service_responce
       JSON.parse(open(SEARCH_URL).read)
     end
