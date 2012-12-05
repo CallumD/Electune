aaf989bce0eb92b9f9b0f830e78acf94e6f34f1b
Callum Dryden <c.dryden@intrallect.com>
2012-12-05 17:20:16 +0000

    Adding some more ideas on performance fixes. This is not working code.

--- app/helpers/spotify_song_search.rb
+++ app/helpers/spotify_song_search.rb
@@ -17,25 +17,51 @@ ARTISTS='artists'
 RELEASED='released'
 
   def self.perform_search search_term
+	  @albums = []
+    @artists = []
+    @songs = []
     service_result = get_service_response search_term
     service_result[TRACKS].map { |service_song| build_song_from_hash service_song }
+
+    @albums.uniq!{|album| album.spotify_link}
+    @artists.uniq!{|artist| artist.spotify_link}
+    @songs.uniq!{|song| song.spotify_link}
+
+    Album.transaction do
+			@albums.each do |album|
+				album.save if album.new_record?
+			end
+		end
+
+		Artist.transaction do
+			@artists.each do |artist|
+				artist.save if artist.new_record?
+			end
+		end
+
+		Song.transaction do
+			@songs.each do |song|
+				song.save if song.new_record?
+			end
+		end
+		@songs
   end
 
   private
     def self.build_song_from_hash data
-      @song = Song.find_by_spotify_link(data[HREF]) || Song.new(name: data[NAME], length: Time.at(data[LENGTH]).utc.strftime(TIME_FORMAT), spotify_link: data[HREF])
-      @song.album = Album.find_by_spotify_link(data[ALBUM][HREF]) || Album.new(name: data[ALBUM][NAME], release_date: data[ALBUM][RELEASED],
+      song = Song.find_or_initialize_by_spotify_link(name: data[NAME], length: Time.at(data[LENGTH]).utc.strftime(TIME_FORMAT), spotify_link: data[HREF])
+      song.album = Album.find_or_initialize_by_spotify_link(name: data[ALBUM][NAME], release_date: data[ALBUM][RELEASED],
       spotify_link: data[ALBUM][HREF])
-      data[ARTISTS].map { |artist| build_artist_from_hash artist, @song }
-      @song.save if @song.new_record?
-      @song
+      @albums << song.album
+      song.artists = data[ARTISTS].map { |artist| build_artist_from_hash artist }
+      @artists << song.artists
+      @songs << song
     end
 
 
-    def self.build_artist_from_hash artist, song
-      artist = Artist.find_by_spotify_link(artist[HREF]) || Artist.new(name: artist[NAME],
+    def self.build_artist_from_hash artist
+      Artist.find_or_initialize_by_spotify_link(name: artist[NAME],
           spotify_link: artist[HREF])
-      song.artists << artist unless song.artists.include? artist
     end
 
     def self.get_service_response search_term
