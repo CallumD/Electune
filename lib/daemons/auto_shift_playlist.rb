37b7dcaf10f5abd41c35865f8b59a73e3682a409
Callum Dryden <callumdryden@gmail.com>
2012-12-16 15:00:05 +0000

    Fixing the playlist javascript url and adding logging to the daemon.

--- lib/daemons/auto_shift_playlist.rb
+++ lib/daemons/auto_shift_playlist.rb
@@ -3,7 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + "/../../config/environment")
 
 loop do
   Playlist.all.each do |playlist|
-    CheckTimeAndShift.tick_on_playlist playlist.id unless playlist.playlist_items.empty?
+    puts "playlist #{playlist.name} items empty? #{playlist.playlist_items.empty?}"
+    result = "no work been done"
+    result = CheckTimeAndShift.tick_on_playlist playlist.id unless playlist.playlist_items.empty?
+    if playlist.playlist_items.empty?
+      puts result
+    else
+      puts "#{result} of #{playlist.playlist_items.first.song.length}"
+    end
   end
   sleep 10
 end
