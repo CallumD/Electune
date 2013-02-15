32826e7e04565987dbfbd6fdc9da1a550baf461e
Callum Dryden <callumdryden@gmail.com>
2013-02-15 20:15:03 +0000

    Released has to be a string as the response is not consistent

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album
   include Mongoid::Document
 
   field :name, type: String
-  field :release_date, type: Time
+  field :release_date, type: String
   field :spotify_link, type: String
   attr_accessible :name, :release_date, :spotify_link
   has_many :songs
