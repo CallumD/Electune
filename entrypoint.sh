34384051cf6c3940dc47eebdb83ca7aa68df9e03
Callum Dryden <callumdryden@gmail.com>
2016-12-11 00:29:11 +0000

    Correting ezstream name typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i "s/<sourcepassword>[^<]*<\/sourcepassword>/<sourcepassword>$ICECAST_SOURCE_PASSWORD<\/sourcepassword>/g" /src/ezstream/ezsteam.xml
+    sed -i "s/<sourcepassword>[^<]*<\/sourcepassword>/<sourcepassword>$ICECAST_SOURCE_PASSWORD<\/sourcepassword>/g" /src/ezstream/ezstream.xml
 fi
 if [ -n "$ICECAST_URL" ]; then
-    sed -i "s/<url>[^<]*<\/url>/<url>$ICECAST_RELAY_PASSWORD<\/url>/g" /src/ezstream/ezsteam.xml
+    sed -i "s/<url>[^<]*<\/url>/<url>$ICECAST_RELAY_PASSWORD<\/url>/g" /src/ezstream/ezstream.xml
 fi
 if [ -n "$ELECTUNE_URL" ]; then
     sed -i "s/http:\/\/localhost:9292/$ELECTUNE_URL/g" /src/ezstream/get_ezstream_filename.sh
