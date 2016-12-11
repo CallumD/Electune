0ea0a92cb58ba80509f23419ed0956c0584241d1
Callum Dryden <callumdryden@gmail.com>
2016-12-11 02:06:26 +0000

    More sed fixes for urls

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
     sed -i "s/<sourcepassword>[^<]*<\/sourcepassword>/<sourcepassword>$ICECAST_SOURCE_PASSWORD<\/sourcepassword>/g" ezstream/ezstream.xml
 fi
 if [ -n "$ICECAST_URL" ]; then
-    sed -i "s/<url>[^<]*<\/url>/<url>$ICECAST_URL<\/url>/g" ezstream/ezstream.xml
+    sed -i "s#<url>[^<]*<\/url>#<url>$ICECAST_URL<\/url>#g" ezstream/ezstream.xml
 fi
 if [ -n "$ELECTUNE_URL" ]; then
     sed -i "s#http://localhost:9292#$ELECTUNE_URL#g" ezstream/get_ezstream_filename.sh
