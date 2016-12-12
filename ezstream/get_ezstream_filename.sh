644339c7577cb864c2b6eea5e3e1b0e02c769ab7
Callum Dryden <callumdryden@gmail.com>
2016-12-12 09:58:43 +0000

    We want to include the quotes on the file name

--- ezstream/get_ezstream_filename.sh
+++ ezstream/get_ezstream_filename.sh
@@ -1,1 +1,1 @@
-curl -X POST -s 'http://localhost:9292/playlists/1/current.json' | grep song_link | sed -e 's/"song_link": "\([^"]*\)"/\1/' | sed -e 's/^[ \t]*//'
+curl -X POST -s 'http://localhost:9292/playlists/1/current.json' | grep song_link | sed -e 's/"song_link": \(.*\)/\1/' | sed -e 's/^[ \t]*//'
