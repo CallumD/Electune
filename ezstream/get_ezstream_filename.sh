672e8a82691c29b10e4c2fddd613dcf7506dee74
Callum Dryden <callumdryden@gmail.com>
2016-12-12 11:21:36 +0000

    Attempted fix for music filename

--- ezstream/get_ezstream_filename.sh
+++ ezstream/get_ezstream_filename.sh
@@ -1,1 +1,2 @@
-curl -X POST -s 'http://localhost:9292/playlists/1/current.json' | grep song_link | sed -e 's/"song_link": \(.*\)/\1/' | sed -e 's/^[ \t]*//'
+file=`curl -X POST -s 'http://localhost:9292/playlists/1/current.json' | grep song_link | sed -e 's/"song_link": "\([^"]*\)"/\1/' | sed -e 's/^[ \t]*//'`
+echo $file
